4f281aeb14796ecec625b7d6792fdfca13756b93
takam1602 <kmtttk1602@gmail.com>
2025-05-06 18:19:14 +0900

    -

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse, type RouteHandlerContext } from 'next/server'
+
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
