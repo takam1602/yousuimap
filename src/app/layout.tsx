5cdeeb7df966dbb0f9869d8e6e25643a0b08eda5
takam1602 <kmtttk1602@gmail.com>
2025-05-19 19:23:15 +0900

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import type { Metadata } from 'next'
+// import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 import HeaderBar from '@/components/HeaderBar'
 import { AuthProvider } from '@/contexts/AuthContext'
